6c303dea5034a73441944624ae06f67c3bffc66d
0n3Appl3 <jedd.lupoy@gmail.com>
2023-12-02 14:51:40 +1300

    Revert "Adding dist"
    This reverts commit 2ee9d4dc49d3bcdbf67190837595c9f4d578849e.

---
+++ src/components/NameTitle.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import ButtonLink from './functionality/ButtonLink.vue';
+
+const buttons = ref([
+    {
+        text: 'About',
+        anchor: '#about',
+    },
+    {
+        text: 'Portfolio',
+        anchor: '#portfolio',
+    },
+    {
+        text: 'Contact',
+        anchor: '#contact',
+    },
+])
+</script>
+
+<template>
+    <div class="name-title__container">
+        <div class="name-title__name">Hi, I'm Jedd.</div>
+        <div class="name-title__title">Software Engineering Graduate</div>
+        <div class="buttons__container">
+            <ButtonLink :text="buttons[0].text" :anchor="buttons[0].anchor" />
+            <ButtonLink :text="buttons[1].text" :anchor="buttons[1].anchor" />
+            <ButtonLink :text="buttons[2].text" :anchor="buttons[2].anchor" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.name-title__container {
+    width: 100%;
+}
+.name-title__name {
+    font-size: 6rem;
+    font-weight: bold;
+    padding-bottom: 0rem;
+}
+.name-title__title {
+    font-size: 3rem;
+}
+.buttons__container {
+    padding-top: 5rem;
+}
+@media screen and (max-width: 850px) {
+    .name-title__title {
+        font-size: 2.5rem;
+    }
+}
+@media screen and (max-width: 700px) {
+    .name-title__name {
+        font-size: 4rem;
+    }
+    .name-title__title {
+        font-size: 2rem;
+    }
+}
+@media screen and (max-width: 500px) {
+    .name-title__name {
+        padding-bottom: 1rem;
+        line-height: 4.5rem;
+    }
+}
+</style>
---
+++ src/components/functionality/ButtonLink.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineProps({
+    text: {
+        type: String,
+        required: true,
+    },
+    anchor: {
+        type: String,
+        default: '#',
+    }
+})
+</script>
+
+<template>
+    <a v-smooth-scroll :href="anchor">{{ text }}</a>
+</template>
+
+<style scoped>
+a {
+    display: inline-block;
+    text-decoration: none;
+    font-size: 1.1rem;
+    padding: 1rem 2rem;
+    margin-right: 1rem;
+    margin-bottom: 0.7rem;
+    border-radius: 2rem;
+    border: 2px solid var(--color-text);
+    background-color: var(--color-text);
+    color: var(--color-background);
+    transition: all 0.15s ease-in-out;
+}
+a:hover {
+    background-color: var(--color-background);
+    color: var(--color-text);
+}
+@media screen and (max-width: 700px) {
+    a {
+        font-size: 0.8rem;
+        padding: 0.6rem 1.2rem;
+        margin-right: 0.7rem;
+        margin-bottom: 0.4rem;
+    }
+}
+</style>
